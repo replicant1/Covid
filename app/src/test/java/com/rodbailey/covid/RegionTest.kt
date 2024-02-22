71c0a3f7b47d43ce67eeae1fe4578b90790785bc
rodbailey <rodney.p.bailey@gmail.com>
2024-02-22 20:51:43 +1100

    Move RegionTest from (androidTest) to (test)

--- app/src/test/java/com/rodbailey/covid/RegionTest.kt
+++ app/src/test/java/com/rodbailey/covid/RegionTest.kt
@@ -1,12 +1,9 @@
 package com.rodbailey.covid
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.rodbailey.covid.dom.Region
 import org.junit.Assert
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class RegionTest {
 
     @Test
