bcb9072ca49497863339de16d7502b6843386eed
rodbailey <rodney.p.bailey@gmail.com>
2024-03-01 15:56:37 +1100

    Added MainScreenTag.kt

--- app/src/main/java/com/rodbailey/covid/domain/Region.kt
+++ app/src/main/java/com/rodbailey/covid/domain/Region.kt
@@ -3,7 +3,7 @@ package com.rodbailey.covid.domain
 import com.google.gson.annotations.SerializedName
 
 /**
- * Geographical region that has a 3 letter ISO code.
+ * Geographical region that has a 3 letter ISO code and a name.
  */
 data class Region(
     @SerializedName("iso")
@@ -12,7 +12,27 @@ data class Region(
     @SerializedName("name")
     val name:String) {
 
+    /**
+     * @return true if the given query string matches against the [name]. If the [name]
+     * of the Region contains multiple words, a query that contains the first letters
+     * of those words (or some substring thereof) will constitute a match.
+     */
     fun matchesSearchQuery(query : String) : Boolean {
-        return name.contains(query, ignoreCase = true)
+        var result = false
+
+        if (name.contains(query, ignoreCase = true)) {
+            result = true
+        }
+        else if (name.contains(" ")) {
+            var firstLetters  = ""
+            name.split(" ").forEach {
+                if (it.isNotEmpty()) {
+                    firstLetters += it[0]
+                }
+            }
+            result = firstLetters.contains(query, ignoreCase = true)
+        }
+
+        return result
     }
 }
--- app/src/test/java/com/rodbailey/covid/RegionTest.kt
+++ app/src/test/java/com/rodbailey/covid/RegionTest.kt
@@ -23,4 +23,19 @@ class RegionTest {
         val region = Region(iso3Code = "RRR", name = "A Region")
         Assert.assertTrue(region.matchesSearchQuery("reg"))
     }
+
+    @Test
+    fun firstLettersMatchMultiWord() {
+        val region = Region("UAE", "United Arab Emirates")
+        Assert.assertTrue(region.matchesSearchQuery("ua"))
+        Assert.assertTrue(region.matchesSearchQuery("ae"))
+        Assert.assertTrue(region.matchesSearchQuery("uae"))
+    }
+
+    @Test
+    fun lastLettersDoNotMatchMultiWord() {
+        val region = Region("UAE", "United Arab Emirates")
+        Assert.assertFalse(region.matchesSearchQuery("da"))
+        Assert.assertFalse(region.matchesSearchQuery("db"))
+    }
 }
